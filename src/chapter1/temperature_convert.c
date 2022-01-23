a65b71c090c02385bac9aaf74dff7a4f027e45ba
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-01-22 19:06:49 -0500

    Use function for Celsius to Fahrenheit conversion

--- src/chapter1/temperature_convert.c
+++ src/chapter1/temperature_convert.c
@@ -5,13 +5,18 @@
 #define STEP 10
 
 int main() {
-    float cels, fahr;
+    float cels, fahr, c_to_f(float);
 
     printf("Celsius\t\tFahrenheit\n");
     for (float n = START; n <= END; n += STEP) {
         cels = n;
-        fahr = (9.0/5.0) * cels + 32.0;
+        fahr = c_to_f(cels);
 
         printf("%.1f\t\t%.1f\n", cels, fahr);
     }
+}
+
+/* Convert from Celsius to Fahrenheit */
+float c_to_f(float celsius) {
+    return (9.0/5.0) * celsius + 32.0;
 }
